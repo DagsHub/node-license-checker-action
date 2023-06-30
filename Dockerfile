0d2be2b5a1c809de6eb911e8fd18349b3a290882
Guy Smoilovsky <guy@dagshub.com>
2023-06-30 15:00:18 +0300

    Entrypoint script works

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# TODO: Node alpine
+FROM alpine:3.10
+
+RUN npm install license-checker
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
+
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+script_dir="$(dirname "$0")"
+default_allowed_licenses="$script_dir/allowed-licenses.txt"
+
+# Default to file if not specified
+ALLOWED_LICENSES="${ALLOWED_LICENSES:-$(cat $default_allowed_licenses | tr '\n' ','| sed 's/,$//')}"
+echo "Allowed licenses: $ALLOWED_LICENSES"
+
+ALL_OUTPUT_FILE="${ALL_OUTPUT_FILE:-licenses.csv}"
+DISALLOWED_OUTPUT_FILE="${DISALLOWED_OUTPUT_FILE:-disallowed-licenses.csv}"
+license-checker-rseidelsohn ${*:---summary --unknown --nopeer --csv} | tee "$ALL_OUTPUT_FILE"
+license-checker-rseidelsohn ${*:---summary --unknown --nopeer --csv --excludeLicenses "$ALLOWED_LICENSES"} > "$DISALLOWED_OUTPUT_FILE"
+
+line_count=$(wc -l < "$DISALLOWED_OUTPUT_FILE")
+if [ "$line_count" -gt 1 ]; then
+    echo "Dangerous dependencies found"
+    cat "$DISALLOWED_OUTPUT_FILE"
+    exit 1
+fi
